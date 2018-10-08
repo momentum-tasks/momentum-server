05e96ff4ec3ca94da6ee3d499ea7cb006e5904ff
Sean Sackowitz <sean@roadturtlegames.com>
2018-10-08 14:25:07 -0400

    Logout functionality

--- web/src/header.jsx
+++ web/src/header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import axios from "axios";
+import { Cookies } from "react-cookie";
 import {
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
-  NavLink
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from "reactstrap";
 
 export class Header extends React.Component {
@@ -25,6 +29,15 @@ export class Header extends React.Component {
     });
   }
 
+  logout = event => {
+    event.preventDefault();
+    axios.post("http://localhost:3000/logout", null, {
+      headers: { "X-Session-Token": this.props.token }
+    });
+    new Cookies().remove("X-Session-Token");
+    window.location.reload();
+  };
+
   render() {
     return (
       <div>
@@ -33,9 +46,16 @@ export class Header extends React.Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink>{this.props.user.Username}</NavLink>
-              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  {this.props.user.Username}
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Settings</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem onClick={this.logout}>Logout</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
